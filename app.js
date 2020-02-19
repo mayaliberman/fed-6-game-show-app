a8819232b11648d28202c81ee1df1ae4739a8a83
unknown <mayaliberman1@gmail.com>
2020-02-19 11:46:40 +0200

    adding restart function

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const phrase = document.getElementById("phrase");
 const startButton = document.querySelector(".btn__reset");
 let missed = 0;
 const phrases = [
-  "A bird in the hand is worth two in the bush",
+  "A bunch of fives",
   "A piece of cake",
   "A fish out of water",
   "All you need is love",
-  "Make my day"
+  "Make my day", 
+  'Fancy-pants'
 ];
 
 function getRandomPhraseAsArray(arr) {
@@ -21,15 +22,15 @@ function addPhraseToDisplay(arr) {
     const ul = phrase.firstElementChild;
     const li = document.createElement("li");
     li.textContent = char;
+    li.style.marginTop = '10px'
     if (char !== " ") {
       li.className = "letter";
+    } else {
+        li.style.paddingLeft = '10px'
     }
     ul.appendChild(li);
   }
 }
-
-const phraseArray = getRandomPhraseAsArray(phrases);
-  addPhraseToDisplay(phraseArray);
 
 function checkLetter(button) {
   const letter = button.textContent;
@@ -49,21 +50,28 @@ function checkWin() {
   const showLetters = document.querySelectorAll(".show");
   const overlay = document.getElementById("overlay");
   const title = document.querySelector("#overlay h2");
+
   if (lettersList.length === showLetters.length) {
     overlay.style.display = "flex";
     overlay.className = "win";
     title.textContent = "You Won!";
+    //RESTARTING THE GAME
+    restartGame();
   } else if (missed > 4) {
     overlay.style.display = "flex";
     overlay.className = "lose";
     title.textContent = "You Lost!";
-  } 
+    //RESTARTING THE GAME
+    restartGame();
+  }
 }
 
 startButton.addEventListener("click", () => {
   const overlay = document.getElementById("overlay");
-    overlay.style.display = "none";
- 
+  overlay.style.display = "none";
+  missed = 0;
+  const phraseArray = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phraseArray);
 });
 
 qwerty.addEventListener("click", e => {
@@ -80,6 +88,28 @@ qwerty.addEventListener("click", e => {
       heart.remove();
     }
   }
-
   checkWin();
 });
+
+function restartGame() {
+    const lis = document.querySelectorAll("#phrase ul li");
+    const scoreboard = document.querySelector("#scoreboard ol");
+    const numOfHeartLeft = scoreboard.children.length;
+    for (let li of lis) {
+      li.remove();
+    }
+    for (let i = 0; i < (5-numOfHeartLeft); i++) {
+      const heart = document.createElement("li");
+      heart.className = "tries";
+      heart.innerHTML =
+        '<img src="images/liveHeart.png" height="35px" width="30px"></img>';
+      scoreboard.appendChild(heart);
+    }
+
+    const keys = document.querySelectorAll(".keyrow button");
+    for (let key of keys) {
+      key.classList.remove("chosen");
+      key.disabled = false;
+    }
+
+}
